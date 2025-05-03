ca3040cfb78404621c44e65a0761f04c268b7de3
vishnu venna <vishnuvardhanreddy122@gmail.com>
2025-05-03 11:35:01 +0530

    added Vishnu

---
+++ src/components/Vishnu.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class Vishnu extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Vishnu is here </h1>
+      </div>
+    );
+  }
+}
+
+export default Vishnu;
