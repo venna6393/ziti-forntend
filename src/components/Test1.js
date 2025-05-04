362264729b07b11446df22e98261f0bb35600e93
vishnu venna <vishnuvardhanreddy122@gmail.com>
2025-05-04 13:04:14 +0530

    test-1 is added

---
+++ src/components/Test1.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class Test1 extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Test 1</h1>
+      </div>
+    );
+  }
+}
+
+export default Test1;
